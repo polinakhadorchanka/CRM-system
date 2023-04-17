d6daaa156579638d706faf58d1d647cc5318eeed
Polina Hodorchenko <polina.khadorchanka@gmail.com>
2023-04-17 12:13:31 +0300

    updated craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -4,6 +4,11 @@ module.exports = {
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
     },
   },
 };
