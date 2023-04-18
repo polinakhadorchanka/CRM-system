9edcaaf2bc5af88dff7239f6e804162f27f75822
Polina Hodorchenko <polina.khadorchanka@gmail.com>
2023-04-18 10:01:30 +0300

    CRM-1
    Added 404 page

--- frontend/src/router/RouteManager.tsx
+++ frontend/src/router/RouteManager.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
-import Vacancies from "../pages/vacancies/Vacancies";
-import Vacancy from "../pages/vacancy/Vacancy";
+import Vacancies from "@pages//vacancies/Vacancies";
+import Vacancy from "@pages/vacancy/Vacancy";
+import NotFound from "@pages/404";
 
 const RouteManager = () => {
   return (
     <Routes>
       <Route path={'/'} element={<Vacancies />} />
       <Route path={'/vacancy/:id'} element={<Vacancy />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
