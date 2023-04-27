53826ca0d53df3178c10d977ed4079b7ae3a8404
Polina Hodorchenko <polina.khadorchanka@gmail.com>
2023-04-27 20:09:02 +0300

    CRM-8

--- frontend/src/pages/vacancies/components/pageManager/PageManager.tsx
+++ frontend/src/pages/vacancies/components/pageManager/PageManager.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useMemo} from 'react';
-import {useTypedSelector} from "@hooks/useTypedSelector";
 import {Link, useParams} from "react-router-dom";
 import {useDispatch} from "react-redux";
+import {animateScroll as scroll} from 'react-scroll';
+import {useTypedSelector} from "@hooks/useTypedSelector";
 import {VACANCIES_ACTION_TYPE} from "@store/types/vacancies";
 
 const PageManager = () => {
@@ -14,7 +15,7 @@ const PageManager = () => {
   const isNext = useMemo(() => pageCount ? pageNum < pageCount : false, [page]);
 
   useEffect(() => {
-    if(!pageCount) {
+    if (!pageCount) {
       dispatch({
         type: VACANCIES_ACTION_TYPE.GET_ERROR,
         payload: {
@@ -22,8 +23,7 @@ const PageManager = () => {
           Value: 'The pageCount parameter is null'
         }
       })
-    }
-    else if(isNaN(pageNum) || pageNum < 1 || pageNum > pageCount) {
+    } else if (isNaN(pageNum) || pageNum < 1 || pageNum > pageCount) {
       dispatch({
         type: VACANCIES_ACTION_TYPE.GET_ERROR,
         payload: {
@@ -34,63 +34,62 @@ const PageManager = () => {
     }
   }, [page]);
 
-  const visiblePages:(number | null)[] = useMemo(() => {
-    if(pageCount) {
-      if(pageCount < 10) {
+  const visiblePages: (number | null)[] = useMemo(() => {
+    if (pageCount) {
+      if (pageCount < 10) {
         const res = [];
 
-        for(let i = 2; i < pageCount; i++)
+        for (let i = 2; i < pageCount; i++)
           res.push(i);
 
         return res;
-      }
-      else if(pageNum < 5) {
+      } else if (pageNum < 5) {
         return [2, 3, 4, 5, 6, 7, null];
-      }
-      else if(pageCount && pageNum > pageCount - 4) {
+      } else if (pageCount && pageNum > pageCount - 4) {
         let it = pageCount - 6;
 
         return [null, it, ++it, ++it, ++it, ++it, ++it];
-      }
-      else {
+      } else {
         let it = pageNum - 2;
 
         return [null, it, ++it, ++it, ++it, ++it, null];
       }
-    }
-    else return [];
+    } else return [];
   }, [page]);
 
   return (
     <div className={'flex flex-row justify-center flex-wrap gap-2 text-xs md:text-sm'}>
       <Link
-        className={`${!isPrev 
-          ? 'cursor-default text-gray-400 border-gray-400 dark:text-gray-500 dark:border-gray-500 ' 
+        className={`${!isPrev
+          ? 'cursor-default text-gray-400 border-gray-400 dark:text-gray-500 dark:border-gray-500 '
           : 'cursor-pointer text-gray-800 border-gray-800 dark:text-gray-200 dark:border-gray-200 ' +
           'hover:border-sky-600 hover:text-sky-600 dark:hover:border-sky-200 dark:hover:text-sky-200'} 
           pl-3 pr-2 py-1 border rounded-l-xl uppercase`}
         to={isPrev ? `/vacancies/${pageNum - 1}` : '/vacancies/prev'}
-        onClick={(e) => {if(!isPrev) e.preventDefault()}}>
+        onClick={(e) => {if (!isPrev) e.preventDefault(); else scroll.scrollToTop({duration: '300'})}}>
         prev
       </Link>
 
       <Link
         className={`cursor-pointer px-2 py-1 border uppercase  
           hover:border-sky-600 hover:text-sky-600 dark:hover:border-sky-200 dark:hover:text-sky-200
-          ${pageNum === 1 ? 'border-sky-600 text-sky-600 dark:border-sky-200 dark:text-sky-200' 
+          ${pageNum === 1 ? 'border-sky-600 text-sky-600 dark:border-sky-200 dark:text-sky-200'
           : 'text-gray-800 border-gray-800 dark:text-gray-200 dark:border-gray-200 '}`}
-        to={`/vacancies/1`}>
+        to={`/vacancies/1`}
+        onClick={() => scroll.scrollToTop({duration: '300'})}>
         1
       </Link>
 
       {visiblePages.map((link, index) => {
-        if(!link) return <div key={index}>...</div>
-        else return <Link key={index}
+        if (!link) return <div key={index}>...</div>
+        else return <Link
+          key={index}
           className={`cursor-pointer px-2 py-1 border uppercase  
           hover:border-sky-600 hover:text-sky-600 dark:hover:border-sky-200 dark:hover:text-sky-200
           ${pageNum === link ? 'border-sky-600 text-sky-600 dark:border-sky-200 dark:text-sky-200'
             : 'text-gray-800 border-gray-800 dark:text-gray-200 dark:border-gray-200 '}`}
-          to={`/vacancies/${link}`}>
+          to={`/vacancies/${link}`}
+          onClick={() => scroll.scrollToTop({duration: '300'})}>
           {link}
         </Link>
       })}
@@ -100,17 +99,18 @@ const PageManager = () => {
           hover:border-sky-600 hover:text-sky-600 dark:hover:border-sky-200 dark:hover:text-sky-200          
           ${pageNum === pageCount ? 'border-sky-600 text-sky-600 dark:border-sky-200 dark:text-sky-200'
           : 'text-gray-800 border-gray-800 dark:text-gray-200 dark:border-gray-200 '}`}
-        to={`/vacancies/${pageCount}`}>
+        to={`/vacancies/${pageCount}`}
+        onClick={() => scroll.scrollToTop({duration: '300'})}>
         {pageCount}
       </Link>
 
       <Link
-        className={`${!isNext 
+        className={`${!isNext
           ? 'cursor-default text-gray-400 border-gray-400 dark:text-gray-500 dark:border-gray-500 '
           : 'cursor-pointer text-gray-800 border-gray-800 dark:text-gray-200 dark:border-gray-200 hover:border-sky-600 hover:text-sky-600 dark:hover:border-sky-200 dark:hover:text-sky-200'} 
           pl-2 pr-3 py-1 border rounded-r-xl uppercase`}
         to={isNext ? `/vacancies/${pageNum + 1}` : '/vacancies/next'}
-        onClick={(e) => {if(!isNext) e.preventDefault()}}>
+        onClick={(e) => {if (!isNext) e.preventDefault(); else scroll.scrollToTop({duration: '300'})}}>
         next
       </Link>
     </div>
--- frontend/src/pages/vacancies/components/vacancyList/VacancyList.tsx
+++ frontend/src/pages/vacancies/components/vacancyList/VacancyList.tsx
@@ -8,25 +8,19 @@ const VacancyList = () => {
 
   return (
     <>
-      {
-        vacancies && vacancies.length === 0
-          ? <div className={'mt-1 md:mt-2 font-light'}>No vacancies found.</div>
-          : <>
-            <div className={'mt-1 md:mt-2 grid grid-cols-1 gap-2'}>
-              {
-                vacancies && vacancies.map((vacancy, index) => {
-                  return <VacancyElement
-                    key={vacancy.ID}
-                    vacancy={vacancy}
-                    divide={index + 1 !== vacancies.length}/>
-                })
-              }
-            </div>
-            <div className={'mt-8 mb-4'}>
-              <PageManager />
-            </div>
-          </>
-      }
+      <div className={'mt-1 md:mt-2 grid grid-cols-1 gap-2'}>
+        {
+          vacancies && vacancies.map((vacancy, index) => {
+            return <VacancyElement
+              key={vacancy.ID}
+              vacancy={vacancy}
+              divide={index + 1 !== vacancies.length}/>
+          })
+        }
+      </div>
+      <div className={'mt-8 mb-4'}>
+        <PageManager/>
+      </div>
     </>
   );
 };
